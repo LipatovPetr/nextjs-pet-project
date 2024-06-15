74e1bed3b14c94d374adc5e9c3a0e75a77fc2705
Petr Lipatov <lipatov3@gmail.com>
2024-06-15 16:53:03 +0300

    fix: navBar css module updated

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = async () => {
   return (
     <div className={s.container}>
       <Link href="/" className={s.logo}>
-        Logo
+        PetrDev
       </Link>
       <Links session={session} />
     </div>
